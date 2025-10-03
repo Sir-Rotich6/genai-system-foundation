b9a839dde441a54fd5375aee2edf8a7a14b67e0d
Enock Rotich <150217147+Sir-Rotich6@users.noreply.github.com>
2025-10-03 12:01:59 +0300

    Dowload logic

---
+++ core_modules/grequests.py
@@ -0,0 +1,10 @@
+import subprocess
+
+def download(directory, filename, base_url='https://raw.githubusercontent.com/Sir-Rotich6/genai-system-foundation/main/'):
+    file_url = f"{base_url}{directory}/{filename}"
+    try:
+        curl_command = f'curl -o {filename} {file_url}'
+        subprocess.run(curl_command, check=True, shell=True)
+        print(f"Downloaded '{filename}' successfully.")
+    except subprocess.CalledProcessError:
+        print(f"Failed to download '{filename}'. Check the URL and your internet connection.")
